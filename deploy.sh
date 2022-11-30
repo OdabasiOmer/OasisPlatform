09283d771c3e19af69066f9f005c05ad0bb30092
OdabasiOmer <odabasi.omer@gmail.com>
2022-11-30 15:31:49 +0100

    removed piwind folder

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OASIS_MODEL_DATA_DIR=<..path to piwind ..>
+export OASIS_MODEL_DATA_DIR=./models/OasisRed
 
 docker rmi coreoasis/api_server:latest
 docker rmi coreoasis/model_worker:latest
