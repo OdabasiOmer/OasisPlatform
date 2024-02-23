2c8ddc5dcf90afdd76c62a7f1d971db6a5493ed5
OdabasiOmer <omer.odabasi@redrisk.com>
2024-02-23 13:20:00 +0100

    patch: deployment adjustments and fixes
    Mainly, we are no longer building the api_server image, instead pulling version 1.26.6.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Important: Set this variable accordingly: set it to the model directory; it overwrites Dockerfile env variable.
-export OASIS_MODEL_DATA_DIR=/data/OasisRed/
+export OASIS_MODEL_DATA_DIR=../OasisRed/
 
 # Note: no need to redo api_server since we dont touch it
 #docker rmi coreoasis/api_server:latest
 docker rmi coreoasis/model_worker:latest
+docker rmi coreoasis/api_server:latest
 docker rmi coreoasis/model_worker:1.26.2
 
-# Note: no need to redo api_server since we dont touch it
-#docker build -f Dockerfile.api_server -t coreoasis/api_server .
+docker pull coreoasis/api_server:1.26.6
 
 # Note: Commenting this out because the worker container is built from inside the .yml 
 # based on Dockerfile.model_worker file, which is fine.
 # docker build -f Dockerfile.model_worker -t coreoasis/model_worker:1.26.2 .
-
 docker-compose -f docker-compose.yml -f oasisui-docker-standalone.yml up -d
 docker-compose -f portainer.yaml up -d
